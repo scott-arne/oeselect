32768f513226d6f65a25c89ab1f2b7054118034b
Scott Arne Johnson <scott.johnson@bms.com>
2026-04-27 17:53:18 -0700

    bump: version 1.3.0 → 1.3.1

--- python/oeselect/__init__.py
+++ python/oeselect/__init__.py
@@ -98,8 +98,8 @@ import re
 import warnings
 
 # Version info
-__version__ = "1.3.0"
-__version_info__ = (1, 3, 0)
+__version__ = "1.3.1"
+__version_info__ = (1, 3, 1)
 
 
 def _ensure_library_compat():
